6d4d8b2c7d34060d4e2ac74efb6f9121a7673764
bradene0 <bradene0@outlook.com>
2024-02-03 21:33:13 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ sanity.config.ts
@@ -0,0 +1,61 @@
+/**
+ * This config is used to set up Sanity Studio that's mounted on the `/pages/studio/[[...index]].tsx` route
+ */
+
+import { debugSecrets } from '@sanity/preview-url-secret/sanity-plugin-debug-secrets'
+import { visionTool } from '@sanity/vision'
+import {
+  apiVersion,
+  dataset,
+  DRAFT_MODE_ROUTE,
+  projectId,
+} from 'lib/sanity.api'
+import { locate } from 'plugins/locate'
+import { previewDocumentNode } from 'plugins/previewPane'
+import { settingsPlugin, settingsStructure } from 'plugins/settings'
+import { defineConfig } from 'sanity'
+import { presentationTool } from 'sanity/presentation'
+import { structureTool } from 'sanity/structure'
+import { unsplashImageAsset } from 'sanity-plugin-asset-source-unsplash'
+import authorType from 'schemas/author'
+import postType from 'schemas/post'
+import settingsType from 'schemas/settings'
+
+const title =
+  process.env.NEXT_PUBLIC_SANITY_PROJECT_TITLE || 'Next.js Blog with Sanity.io'
+
+export default defineConfig({
+  basePath: '/studio',
+  projectId,
+  dataset,
+  title,
+  schema: {
+    // If you want more content types, you can add them to this array
+    types: [authorType, postType, settingsType],
+  },
+  plugins: [
+    structureTool({
+      structure: settingsStructure(settingsType),
+      // `defaultDocumentNode` is responsible for adding a “Preview” tab to the document pane
+      defaultDocumentNode: previewDocumentNode(),
+    }),
+    presentationTool({
+      locate,
+      previewUrl: {
+        draftMode: {
+          enable: DRAFT_MODE_ROUTE,
+        },
+      },
+    }),
+    // Configures the global "new document" button, and document actions, to suit the Settings document singleton
+    settingsPlugin({ type: settingsType.name }),
+    // Add an image asset source for Unsplash
+    unsplashImageAsset(),
+    // The remaining plugins are only loaded in dev mode
+    process.env.NODE_ENV !== 'production' && debugSecrets(),
+    // Vision lets you query your content with GROQ in the studio
+    // https://www.sanity.io/docs/the-vision-plugin
+    process.env.NODE_ENV !== 'production' &&
+      visionTool({ defaultApiVersion: apiVersion }),
+  ],
+})
